2f268d51a0a64a93c70400d904dc985ee3bac341
Charlie Tuttle <charlietuttle@Charlies-MacBook-Pro.local>
2017-01-21 23:43:45 -0800

    Added models for the tablesetting app

--- tablesetter/models.py
+++ tablesetter/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class Table(models.Model):
+	table_sponsor = models.ForeignKey(User, on_delete=models.CASCADE)
+	table_size = models.IntegerField(blank=False)
+
+	def __str__(self):
+		return table_sponsor
+
+class Guest(models.Model):
+	table = models.ForeignKey(Table, on_delete=models.CASCADE)
+	guest_first_name = models.CharField(max_length=50, blank=False)
+	guest_last_name = models.CharField(max_length=50, blank=False)
+	guest_email = models.EmailField(max_length = 50, blank=False)
+
+	def __str__(self):
+		return "%s %s" % (guest_first_name, guest_last_name)
