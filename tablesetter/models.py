966577da8a1ed4c07d2926f9b25eb0935692dd7f
Charlie Tuttle <charlietuttle@Charlies-MacBook-Pro.local>
2017-01-23 20:25:33 -0800

    updated the models

--- tablesetter/models.py
+++ tablesetter/models.py
@@ -2,34 +2,57 @@ from django.db import models
 from accounts.models import User
 from django.core.validators import MaxValueValidator
 
-# Create your models here.
 
-'''
 class Gala(models.Model):
-	gala_name = models.CharField(max_length=100)
-	gala_date = models.DateTimeField('event date')
-	gala_num_tables = models.IntegerField()
-	gala_total_guests = models.IntegerField()
+	gala_name = models.CharField('event name', max_length=100, blank=False)
+	gala_datetime = models.DateTimeField('event date', blank=False)
+	gala_num_tables = models.IntegerField('number of tables',)
+	gala_total_guests = models.IntegerField('maximum number of guests',)
+	date_created = models.DateTimeField('date created', auto_now_add=True, auto_now=False, null=True)
+	date_updated = models.DateTimeField('date updated', auto_now_add=False, auto_now=True, null=True)
 
 	def __str__(self):
 		return gala_name
 
+class MealChoice(models.Model):
+	gala = models.ForeignKey(Gala, on_delete=models.CASCADE)
+	choice_name = models.CharField('meal choice', max_length=200)
+
+	def __str__(self):
+		return self.choice_name
+
+#In this class, our 'user' is basically a table sponsor who buys the table at the gala
 class Table(models.Model):
 	gala = models.ForeignKey(Gala, on_delete=models.CASCADE)
-	table_sponsor = models.ForeignKey(User, on_delete=models.CASCADE)
-	table_size = models.IntegerField(blank=False, validators=[MaxValueValidator(100)])
+	user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	table_sponsor_name = models.CharField('table sponsor', max_length=150, blank=False)
+	table_size = models.IntegerField(blank=False, validators=[MaxValueValidator(99)])
+	date_created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
+	date_updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 
 	def __str__(self):
 		return table_sponsor
 
-class Guest(models.Model):
+class Party(models.Model):
 	table = models.ForeignKey(Table, on_delete=models.CASCADE)
-	guest_first_name = models.CharField(max_length=50, blank=False, null=True)
-	guest_last_name = models.CharField(max_length=50, blank=False, null=True)
-	guest_email = models.EmailField(max_length = 200, blank=False, null=True)
-	has_replied = models.BooleanField(default=False)
-	dietary_restrictions = models.CharField(max_length=300, blank=True, null=True)
+	party_contact = models.CharField('full name of party contact', max_length=150)
 
 	def __str__(self):
-		return "%s %s" % (guest_first_name, guest_last_name)
-		'''
+		return self.party_contact
+
+class Seat(models.Model):
+	table = models.ForeignKey(Table, on_delete=models.CASCADE)
+	party = models.ForeignKey(Party, on_delete=models.CASCADE, null=True)
+	details_completed = models.BooleanField(default=False)
+	is_donated = models.BooleanField(default=False)
+	seat_number = models.IntegerField('seat number')
+
+class SeatDetails(models.Model):
+	seat = models.OneToOneField(Seat)
+	guest_first_name = models.CharField(max_length=50, blank=False)
+	guest_last_name = models.CharField(max_length=50, blank=False)
+	meal_choice = models.CharField('meal choice', max_length=200, blank=False)
+	dietary_restrictions = models.CharField('dietary restrictions', max_length=300, blank=True)
+	special_accommodations = models.CharField(max_length=300, blank=True)
+	initial_response_date = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
+	date_updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
